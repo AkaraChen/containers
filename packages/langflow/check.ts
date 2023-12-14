753870bc718fd81ca8068cda30afd3ad08b46f64
AkaraChen <akarachen@outlook.com>
2023-12-14 21:40:35 +0800

    refactor: refactor

--- packages/langflow/check.ts
+++ packages/langflow/check.ts
@@ -1,19 +1,13 @@
 import { $ } from "zx";
 
-async function main() {
-  const pipVersion = (
-    await $`pip index versions langflow | grep "langflow" | awk '{print $2}' | tr -d '()'`.quiet()
-  ).stdout.trim();
+const pipVersion = (
+  await $`pip index versions langflow | grep "langflow" | awk '{print $2}' | tr -d '()'`.quiet()
+).stdout.trim();
 
-  try {
-    await $`docker image inspect akarachan/langflow:${pipVersion}`.quiet();
-  } catch {
-    // Image not exists, build it
-    console.log(pipVersion);
-    process.exit(0);
-  }
-  // Image exists, do nothing
-  process.exit(1);
+try {
+  await $`docker image inspect akarachan/langflow:${pipVersion}`.quiet();
+} catch {
+  console.log(pipVersion);
+  process.exit(0);
 }
-
-main();
+process.exit(1);
