8f9fb83101f2f157f8956208b24d86de8aa92095
AkaraChen <akarachen@outlook.com>
2023-12-14 22:38:03 +0800

    style: lint
    style: lint

---
+++ containers/langflow/check.ts
@@ -0,0 +1,13 @@
+import { $ } from "zx";
+
+const pipVersion = (
+    await $`pip index versions langflow | grep "langflow" | awk '{print $2}' | tr -d '()'`.quiet()
+).stdout.trim();
+
+try {
+    await $`docker image inspect akarachan/langflow:${pipVersion}`.quiet();
+} catch {
+    console.log(pipVersion);
+    process.exit(0);
+}
+process.exit(1);
